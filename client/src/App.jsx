f96c8d673c687d9cf7c34eb576226265fe592a9f
Abhinav <abhinavgtechnical@gmail.com>
2024-12-07 12:39:18 +0530

    Improved App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter , RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import {
   HomeLayout,
   Landing,
@@ -11,102 +11,98 @@ import {
   Admin,
   Profile,
   AllJobs,
-  EditJob
-} from './pages';
+  EditJob,
+} from "./pages";
 import { action as RegisterAction } from "./pages/Register";
-import { action as LoginAction} from "./pages/Login";
+import { action as LoginAction } from "./pages/Login";
 import { loader as DashboardLoader } from "./pages/DashboardLayout";
 import { action as Addjob } from "./pages/AddJob";
-import {loader as allJobsloader} from "./pages/AllJobs";
+import { loader as allJobsloader } from "./pages/AllJobs";
 
-import { loader as editJobLoader } from './pages/EditJob';
-import { action as editJobAction } from './pages/EditJob';
-import { action as deleteJobAction } from './pages/DeleteJob';
+import { loader as editJobLoader } from "./pages/EditJob";
+import { action as editJobAction } from "./pages/EditJob";
+import { action as deleteJobAction } from "./pages/DeleteJob";
 
-import { loader as adminLoader } from './pages/Admin';
-import { action as profileAction } from './pages/Profile';
-import { loader as statsLoader } from './pages/Stats';
-
+import { loader as adminLoader } from "./pages/Admin";
+import { action as profileAction } from "./pages/Profile";
+import { loader as statsLoader } from "./pages/Stats";
 
 const router = createBrowserRouter([
   {
-      path: '/',
-      element: <HomeLayout />,
-      errorElement: <Error />,
-      children: [
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
       {
         index: true,
         element: <Landing />,
       },
       {
-        path: 'register',
+        path: "register",
         element: <Register />,
-        action:RegisterAction
+        action: RegisterAction,
       },
-      
+
       {
-        path: 'dashboard',
+        path: "dashboard",
         element: <DashboardLayout />,
-        loader:DashboardLoader,
+        loader: DashboardLoader,
         children: [
           {
             index: true,
             element: <AddJob />,
-            action:Addjob
+            action: Addjob,
           },
-          { path: 'stats',
-            element: <Stats />,
-            loader: statsLoader
-          },
+          { path: "stats", element: <Stats />, loader: statsLoader },
           {
-            path: 'all-jobs',
+            path: "all-jobs",
             element: <AllJobs />,
-            loader:allJobsloader,
+            loader: allJobsloader,
           },
 
           {
-            path: 'profile',
+            path: "profile",
             element: <Profile />,
-            action:profileAction
+            action: profileAction,
           },
           {
-            path: 'admin',
+            path: "admin",
             element: <Admin />,
             loader: adminLoader,
           },
           {
-            path: 'edit-job/:id',
-            element: <EditJob/>,
+            path: "edit-job/:id",
+            element: <EditJob />,
             loader: editJobLoader,
             action: editJobAction,
           },
-          { 
-            path: 'delete-job/:id',
-            action: deleteJobAction 
+          {
+            path: "delete-job/:id",
+            action: deleteJobAction,
           },
         ],
       },
       {
-        path: 'login',
+        path: "login",
         element: <Login />,
-        action:LoginAction
+        action: LoginAction,
       },
     ],
   },
 ]);
 const App = () => {
-  const checkDefaultTheme = () => {
-    const isDarkTheme =
-      localStorage.getItem('darkTheme') === 'true'
-    document.body.classList.toggle('dark-theme', isDarkTheme);
-    return isDarkTheme;
-  };
-  
-  const isDarkThemeEnabled = checkDefaultTheme();
-  return <>
-  <RouterProvider router={router}/>
-  </>
-  
-}
+  // const checkDefaultTheme = () => {
+  //   const isDarkTheme = localStorage.getItem("darkTheme") === "true";
+  //   document.body.classList.toggle("dark-theme", isDarkTheme);
+  //   return isDarkTheme;
+  // };
 
-export default App
+  // const isDarkThemeEnabled = checkDefaultTheme();
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+};
+
+export default App;
